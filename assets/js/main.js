d9bb527d027a9dc8be22689679e623d36a530b30
dennisooki <dennisooki@gmail.com>
2022-06-26 17:53:26 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-// *** Write your code here***
+
+const display = document.querySelector('#display')
+const evaluate = document.querySelector('#calculate')
+const Delete = document.querySelector('#del')
+const DeleteAll = document.querySelector('#clear')
+
+function appendNum(x) { 
+     display.value = display.value+x; 
+ }
+ 
+ evaluate.addEventListener('click', () => { 
+     let equation = display.value.trim() 
+     display.value = eval(equation);     
+ })
+
+ Delete.addEventListener('click', () => {display.value = display.value.slice(0,-1);})
+
+ DeleteAll.addEventListener('click',() =>{display.value=''})
+ 
+
